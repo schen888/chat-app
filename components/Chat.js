e1ae29cd33f844166476fa181d9c58e60f3c46ec
schen888 <shuang.chen888@hotmail.com>
2022-12-08 10:21:25 +0100

    Add netInfo function to check if online and render InputToolbar only when online.

--- components/Chat.js
+++ components/Chat.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 import firebase from 'firebase';
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import NetInfo from '@react-native-community/netinfo';
 
 export default class Chat extends React.Component {
   constructor() {
@@ -36,19 +37,6 @@ export default class Chat extends React.Component {
     }), ()=>{
       ()=>this.addMessage()
     })
-  }
-
-  renderBubble(props) {
-    return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: '#000'
-          }
-        }}
-      />
-    )
   }
 
   onCollectionUpdate = (querySnapshot) => {
@@ -115,6 +103,15 @@ export default class Chat extends React.Component {
   componentDidMount(){
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+
+    //use netInfo to check if online or offline
+    NetInfo.fetch().then(connection => {
+      if (connection.isConnected) {
+        console.log('online');
+      } else {
+        console.log('offline');
+      }
+    });
     
     //Authenticate user
     this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
@@ -148,6 +145,32 @@ export default class Chat extends React.Component {
     this.unsubscribe();
     // stop listening to authentication
     this.authUnsubscribe();
+  }
+
+  //Set color of message bubble
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
+  }
+
+  //Only render default inputToolbar when online
+  renderInputToolbar(props) {
+    if (this.state.isConnected == false) {
+    } else {
+      return(
+        <InputToolbar
+        {...props}
+        />
+      );
+    }
   }
 
   render() {
